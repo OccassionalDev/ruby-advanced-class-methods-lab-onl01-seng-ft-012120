5db11f0ce138e890d074f9f03b4bba31904786e6
Erica Schimmel <ericaschimmel01@gmail.com>
2020-01-31 00:40:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,7 @@ class Song
   end 
   
   def self.find_or_create_by_name(name)
-    if (!find_by_name(name)) ? return Song.create_by_name(name): return Song.find_by_name()
+    if (!find_by_name(name)) ? return Song.create_by_name(name) : return Song.find_by_name()
   end 
 
 end
