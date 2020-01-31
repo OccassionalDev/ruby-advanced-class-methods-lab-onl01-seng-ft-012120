9c88b1e086fb12dac74744108d14106bff70935a
Erica Schimmel <ericaschimmel01@gmail.com>
2020-01-31 00:56:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -58,6 +58,8 @@ class Song
   def self.new_from_filename(file_name)
     song_file_parts = file_name.split(" - ")
     binding.pry
+    
+    song_without_mp3 = song_file_parts[1].split(".mp3")
   end 
 
 end
